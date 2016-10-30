86313434fe0797e818c9098468c5b0cf4cb61118
helloram52 <helloram52@gmail.com>
2016-10-30 15:19:02 -0500

    Added LockfreeList changes

--- src/com/multicore/LockFreeList.java
+++ src/com/multicore/LockFreeList.java
@@ -3,8 +3,15 @@ package com.multicore;
 import java.util.concurrent.atomic.AtomicMarkableReference;
 
 public class LockFreeList extends BasicLinkedList {
+  LockFreeNode head, tail;
+
   LockFreeList() {
     super();
+    head = new LockFreeNode(Integer.MIN_VALUE);
+    tail = new LockFreeNode(Integer.MAX_VALUE);
+
+    head.next = new AtomicMarkableReference<>(tail, false);
+    tail.next = new AtomicMarkableReference<>(null, false);
   }
 
   public Window find(LockFreeNode head, int key) {
@@ -48,7 +55,9 @@ public class LockFreeList extends BasicLinkedList {
       else {
         LockFreeNode node = new LockFreeNode(key);
         node.setNext(new AtomicMarkableReference<LockFreeNode>(curr, false));
-        if( pred.getNextAtomicReference().compareAndSet(curr, ) )
+        if( pred.getNextAtomicReference().compareAndSet(curr, node, false, false) ) {
+          return true;
+        }
       }
     }
 
@@ -56,11 +65,35 @@ public class LockFreeList extends BasicLinkedList {
 
   @Override
   public boolean delete(int key) {
-    return true;
+
+    boolean result = true;
+    while(true) {
+      Window window = find(head, key);
+      LockFreeNode pred = window.pred, curr = window.curr;
+      if( curr.getKey() != key ) {
+        return false;
+      }
+      else {
+        LockFreeNode succ = curr.getNext();
+        result = curr.getNextAtomicReference().attemptMark(succ, true);
+        if( !result ) {
+          continue;
+        }
+        pred.getNextAtomicReference().compareAndSet(curr, succ, false, false);
+        return true;
+      }
+    }
   }
 
   @Override
   public boolean search(int key) {
-    return true;
+
+    boolean[] marked = {false};
+    LockFreeNode curr = head;
+    while( curr.getKey() < key ) {
+      curr = curr.getNext();
+      LockFreeNode succ = curr.getNextAtomicReference().get(marked);
+    }
+    return curr.getKey() == key && !marked[0];
   }
 }
